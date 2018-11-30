478f73e2bc9ea00c6a10154384e8d44ad8863bc8
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2018-11-30 21:46:27 +0000

    *Added a script that allows the package to be built from the Editor, and also CI via the command line

---
+++ Assets/DebugOnly/Editor/DebugOnlyPackageBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Builds the DebugOnly package, also used by CI
+/// </summary>
+public static class DebugOnlyPackageBuilder
+{
+    /// <summary>
+    /// Builds the package
+    /// </summary>
+    [MenuItem("Tools/DebugOnly/Build Package")]
+    public static void Build()
+    {
+        var allAssetsInProject = AssetDatabase.GetAllAssetPaths().ToList();
+        //Remove all assets that are not in the assets folder from the package
+        allAssetsInProject.RemoveAll(path => !path.StartsWith("Asset"));
+        //Remove this script from the package as well, as this is used to build
+        allAssetsInProject.Remove("Assets/DebugOnly/Editor/DebugOnlyPackageBuilder.cs");
+
+        //Saves the package to the project root
+        var exportPath = Application.dataPath + "DebugOnly.unitypackage";
+        AssetDatabase.ExportPackage(allAssetsInProject.ToArray(), exportPath);
+        Debug.Log("Exported Package To: " + exportPath);
+    }
+}
