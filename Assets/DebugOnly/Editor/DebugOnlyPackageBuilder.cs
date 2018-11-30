b9d2dae8a918f0d07b57bee65e0cc68144adb1fa
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2018-11-30 21:53:22 +0000

    *Fixed the path the package was exported to *Removed the "Assets" folder from the package, as this would leave an empty folder at the top of the Import Package tree view when importing

--- Assets/DebugOnly/Editor/DebugOnlyPackageBuilder.cs
+++ Assets/DebugOnly/Editor/DebugOnlyPackageBuilder.cs
@@ -16,11 +16,14 @@ public static class DebugOnlyPackageBuilder
         var allAssetsInProject = AssetDatabase.GetAllAssetPaths().ToList();
         //Remove all assets that are not in the assets folder from the package
         allAssetsInProject.RemoveAll(path => !path.StartsWith("Asset"));
+
+        //Remove the assets folder, if not, then there will an empty Assets folder at the top of the import package tree view
+        allAssetsInProject.Remove("Assets");
         //Remove this script from the package as well, as this is used to build
         allAssetsInProject.Remove("Assets/DebugOnly/Editor/DebugOnlyPackageBuilder.cs");
 
         //Saves the package to the project root
-        var exportPath = Application.dataPath + "DebugOnly.unitypackage";
+        var exportPath = Application.dataPath.Replace("Assets", "") + "DebugOnly.unitypackage";
         AssetDatabase.ExportPackage(allAssetsInProject.ToArray(), exportPath);
         Debug.Log("Exported Package To: " + exportPath);
     }
