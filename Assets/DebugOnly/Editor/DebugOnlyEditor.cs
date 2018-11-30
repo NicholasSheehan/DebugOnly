cfb4b2d6c355a820a0e629a821b31da08904319a
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2018-11-30 20:58:45 +0000

    *Initial Commit

---
+++ Assets/DebugOnly/Editor/DebugOnlyEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public class DebugOnlyEditor : Editor
+{
+    /// <summary>
+    /// Sets the debug tag
+    /// </summary>
+    static DebugOnlyEditor()
+    {
+        //there is no type to cast to, so we just get all tag manager assets, in which there is only one, and load it as a serializedObject
+        var tagManagerAssetObject = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0];
+        var tagManagerAsset = new SerializedObject(tagManagerAssetObject);
+        var tagsProperty = tagManagerAsset.FindProperty("tags");
+
+        //First check if the tag is already present
+        var foundDebugTag = false;
+        for (var i = 0; i < tagsProperty.arraySize; i++)
+        {
+            var tag = tagsProperty.GetArrayElementAtIndex(i);
+            if (tag.stringValue.Equals(DebugOnlyProcessor.debugOnlyTag))
+            {
+                foundDebugTag = true;
+                break;
+            }
+        }
+
+        //if the tag was found, then we don't need to add it
+        if (foundDebugTag) return;
+
+        //insert entry into tag list, and then set the tag value, then save
+        tagsProperty.InsertArrayElementAtIndex(0);
+
+        var newTagIndex = tagsProperty.GetArrayElementAtIndex(0);
+        newTagIndex.stringValue = DebugOnlyProcessor.debugOnlyTag;
+
+        tagManagerAsset.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/DebugOnly/Editor/DebugOnlyProcessor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugOnlyProcessor : IProcessScene
+{
+    public const string debugOnlyTag = "DebugOnly";
+
+    public int callbackOrder { get; private set; }
+
+    public void OnProcessScene(Scene scene)
+    {
+#if DEBUG
+        return;
+#endif
+        var sceneRootObjects = scene.GetRootGameObjects();
+
+        foreach (var rootObject in sceneRootObjects)
+        {
+            var children = rootObject.GetComponentsInChildren<Transform>(true);
+
+            foreach (var child in children)
+            {
+                if (child.CompareTag(debugOnlyTag)) Object.DestroyImmediate(child.gameObject);
+            }
+        }
+    }
+}
