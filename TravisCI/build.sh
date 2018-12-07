fff259a014afa6708305fc2bcbf50c918e29e3b4
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2018-12-07 20:44:44 +0000

    *removed commented code

--- TravisCI/build.sh
+++ TravisCI/build.sh
@@ -16,33 +16,6 @@ activateLicense() {
         -quit
 }
 
- # prepareBuilds() {
-     # echo "Preparing building"
-
-     # mkdir ${BUILD_PATH}
-     # echo "Created directory: ${BUILD_PATH}"
- # }
-
-# buildiOS() {
-    # echo "Building ${UNITY_PROJECT_NAME} for iOS"
-
-    # ${UNITY_PATH} \
-        # -batchmode \
-        # -silent-crashes \
-        # -logFile "${TRAVIS_BUILD_DIR}/unity.build.ios.log" \
-        # -projectPath "${TRAVIS_BUILD_DIR}/${UNITY_PROJECT_NAME}" \
-        # -executeMethod Syng.Builds.Build \
-        # -syngBuildPath "${BUILD_PATH}/iOS" \
-        # -quit
-
-    # rc1=$?
-    # echo "Build logs (iOS)"
-    # cat "${TRAVIS_BUILD_DIR}/unity.build.ios.log"
-
-    # # exit if build failed
-    # if [ $rc1 -ne 0 ]; then { echo "Build failed"; exit $rc1; } fi
-# }
-
 buildPackage() {
 	echo "Project Path: ${TRAVIS_BUILD_DIR}"
 	ls "${TRAVIS_BUILD_DIR}"
@@ -73,20 +46,11 @@ returnLicense() {
         -quit
 }
 
-echo "${TRAVIS_BUILD_DIR} Contents:"
-ls "${TRAVIS_BUILD_DIR}"
-
 activateLicense
-#prepareBuilds
-#buildiOS
 buildPackage
 returnLicense
 
 echo "${TRAVIS_BUILD_DIR} Contents:"
 ls "${TRAVIS_BUILD_DIR}"
-
-echo ""
-echo "${TRAVIS_BUILD_DIR}/Assets Contents:"
-ls "${TRAVIS_BUILD_DIR}/Assets"
 
 exit 0
